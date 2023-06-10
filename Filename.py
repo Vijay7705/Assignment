e9c136377e4f2aeef2a44012ab87e467b4a5d1b6
Vijay7705 <136181598+Vijay7705@users.noreply.github.com>
2023-06-10 21:20:30 +0530

    Created Filename.py

---
+++ Filename.py
@@ -0,0 +1,8 @@
+file=input("Input the filename")
+f=file.split(".")
+if(f[-1]=='py'):
+    print("The extension of the file is:'python'")
+elif(f[-1]=='c'):
+    print("The extension of the file is:'C'")
+
+
