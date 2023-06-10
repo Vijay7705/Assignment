80f572b06a83001297f6e9c0cd4ed256d5cd0d7d
Vijay7705 <136181598+Vijay7705@users.noreply.github.com>
2023-06-10 21:26:04 +0530

    Update Filename.py

--- Filename.py
+++ Filename.py
@@ -1,8 +1,10 @@
 file=input("Input the filename")
 f=file.split(".")
+#f[-1] returns the extension as ."ext"
 if(f[-1]=='py'):
     print("The extension of the file is:'python'")
 elif(f[-1]=='c'):
     print("The extension of the file is:'C'")
-
+elif(f[-1]=='java'):
+    print("The extension of the file :",f[-1])
 
