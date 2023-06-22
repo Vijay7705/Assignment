85eeef06ac2f35ef1f30f7e4bcc700327ad20033
Vijay7705 <136181598+Vijay7705@users.noreply.github.com>
2023-06-22 21:59:20 +0530

    Update list1.py

--- list1.py
+++ list1.py
@@ -3,9 +3,18 @@ l=[]
 for i in range(n):
     no=int(input("Enter a number:"))
     l.append(no)
-print("Input list:"l)
+print("Input list:",l)
+#normal output
 for x in l:
     if x>0:
         print(x,end=" ")
     else:
         continue
+#output as a list
+lo=[]
+for x in l:
+    if x>0:
+        lo.append(x)
+    else:
+        continue
+print("\n",lo)
