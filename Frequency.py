77e5f0b41da5acf04aefe85372edd539b99ee03b
Vijay7705 <136181598+Vijay7705@users.noreply.github.com>
2023-06-29 12:48:22 +0530

    Create Frequency.py
    ASSIGNMENT

---
+++ Frequency.py
@@ -0,0 +1,22 @@
+def printChar(arr, Len):
+    freq = {}
+    for i in range(Len):
+         
+        if(arr[i] in freq):
+            freq[arr[i]] = freq[arr[i]] + 1
+        else:
+            freq[arr[i]] = 1
+    size = len(freq)
+    while (size > 0):
+        currentMax = 0
+        arg_max = 0
+        for key, value in freq.items():
+            if (value > currentMax or (value == currentMax and key > arg_max)):
+                arg_max = key
+                currentMax = value
+        print(f"{arg_max} : {currentMax}")
+        freq.pop(arg_max)
+        size -= 1
+Str = input("Enter:")
+Len = len(Str)
+printChar(list(Str), Len)
